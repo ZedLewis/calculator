226c2c295f94b4317be5f85aef6b825edca6be69
Lewis <lewisholliday@hotmail.co.uk>
2021-09-29 20:33:43 +0100

    Moved onto JS now. Having issue updating the display, need to debug.

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
-const numberButtons = document.getElementsByClassName('number')
-const operatorButtons = document.getElementsByClassName('operator')
-const display = document.getElementById('screen')
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const clearButton = document.querySelector('[data-clear]')
+const previousInputElement = document.querySelector('[data-previous-input]')
+const currentInputElement = document.querySelector('[data-current-input]')
 
-let displayNumber = '';
+
+
+
+class Calculator {
+  constructor(previousInputElement, currentInputElement) {
+    this.previousInputElement = previousInputElement
+    this.currentInputElement = currentInputElement
+  }
+
+  delete () {
+
+  }
+
+  clear () {
+    this.previousInputElement = ''
+    this.currentInputElement = ''
+    this.operation = undefined
+
+  }
+
+  /*this should in theory work with the update display function to populate the display in calculator, but not currently working. Trying to debug*/
+  appendNumber(number) {
+    this.currentOperand = number
+
+  }
+
+  choseOperator (operator) {
+
+  }
+
+  calculate() {
+
+  }
+
+  updateDisplay() {
+    this.currentInputElement.innerText = this.currentOperand
+
+  }
+}
+
+const calculator = new Calculator (previousInputElement, currentInputElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
 
 function add (a, b) {
 return a + b;
@@ -19,18 +70,6 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
-
-function appendNumber (number) {
-
-}
-
-numberButtons.forEach((button) => 
-  button.addEventListener('click', () => appendNumber(display.textContent))
-)
-
-operatorButtons.forEach((button) => 
-  button.addEventListener('click', () => appendNumber(display.textContent))
-)
 
 
 
