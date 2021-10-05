1e0b0924b27682055010062180572f8918341f64
Lewis <lewisholliday@hotmail.co.uk>
2021-10-05 19:07:22 +0100

    can now operate both displays and call upon operators - issue calling equals function, not being recognised

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-// const numberButtons = document.querySelectorAll('[data-number]')
-
+const numberButtons = document.querySelectorAll('[data-number]')
 const operatorButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 const clearButton = document.querySelector('[data-clear]')
 const previousInputElement = document.querySelector('[data-previous]')
 const currentInputElement = document.querySelector('[data-current]')
-console.log(currentInputElement)
 
-window.onload = function main() {
-  const numberButtons = document.querySelectorAll('[data-number]')
-
-  numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      console.log(button, "clicked")
-      calculator.appendNumber(button.innerText)
-      calculator.updateDisplay()
-    })
-  })
-  
-}
-
-
+console.log(equalsButton)
 
 class Calculator {
   constructor(previousInputElement, currentInputElement) {
     this.previousInputElement = previousInputElement
     this.currentInputElement = currentInputElement
+    this.currentOperand = ''
+    this.previousOperand = ''
   }
 
   delete () {
@@ -39,27 +26,80 @@ class Calculator {
     this.operation = undefined
 
   }
+// Function is not being recognised when called upon, trying to debug
+  compute() {
+    let result
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if(isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+      case '+':
+        result = prev + current
+        break
+      case '-':
+        result = prev - current
+        break
+      case 'x':
+        result = prev * current
+        break
+      case '÷':
+        result = prev / current
+        break 
+      default:
+        return
+    }
+    this.currentOperand = result
+    this.operation = undefined
+    this.previousOperand = ''
+  }
 
-  /*this should in theory work with the update display function to populate the display in calculator, but not currently working. Trying to debug*/
+  // Function ends if more than one decimal with return
   appendNumber(number) {
-    this.currentOperand = number
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+    
+  }
+
+  choseOperator(operator) {
+    if (this.currentOperand === '') return
+    if (this.previousOperand !== '') {
+      this.compute()
+    }
+    this.operator = operator
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ''
 
   }
 
-  choseOperator (operator) {
-
-  }
-
-  calculate() {
-
-  }
 
   updateDisplay() {
-    console.log (this)
     this.currentInputElement.innerText = this.currentOperand
+    this.previousInputElement.innerText = this.previousOperand
 
   }
 }
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log(button, "clicked")
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+operatorButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log(button, "clicked")
+    calculator.choseOperator(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+
+equalsButton.addEventListener('click', () => {
+  calculator.compute()
+  calculator.updateDisplay()
+})
 
 const calculator = new Calculator (previousInputElement, currentInputElement)
 
